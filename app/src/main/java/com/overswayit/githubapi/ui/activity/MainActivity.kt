b80f22f24c24ea0d9f80ae82aed99577caf65027
Lazar Ristic <lazar.ristic@vibe.rs>
2020-08-30 22:52:28 +0200

    :bug: Fixing Action bar not showing current tittle

--- app/src/main/java/com/overswayit/githubapi/ui/activity/MainActivity.kt
+++ app/src/main/java/com/overswayit/githubapi/ui/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.overswayit.githubapi.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.overswayit.githubapi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val appBarConfig = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfig)
     }
 }
